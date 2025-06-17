fedb754e223b2e4e1faa1757d89628e4538f6493
SHAILENDRA SHUKLA <SHAILENDRAIIITS@GMAIL.COM>
2025-06-17 18:43:59 +0530

    Added Joi validation for signup

---
+++ backend/validations/auth.validation.js
@@ -0,0 +1,19 @@
+const { profile } = require("console");
+const Joi=require("joi");
+
+const signUpValidation= Joi.object().keys({
+  name: Joi.string().required().trim(),
+  username: Joi.string().required().trim(),
+  email: Joi.string().email().required().trim(),
+  password: Joi.string().min(6).required().trim(),
+  role: Joi.string().valid("guide", "learner").required(),
+  profile: Joi.object().({
+        expertise: Joi.array().items(Joi.string()).optional(),
+        interests: Joi.array().items(Joi.string()).optional(),
+        experience: Joi.string().optional(),
+    }).optional(),
+});
+
+module.exports{
+    signUpValidation
+}
