d3ada3dce76fc91af76a538fa457d305773f2dbd
SHAILENDRA SHUKLA <SHAILENDRAIIITS@GMAIL.COM>
2025-06-18 00:25:49 +0530

    added controller for listing guides by particular usernam

--- backend/controllers/guide.controller.js
+++ backend/controllers/guide.controller.js
@@ -15,6 +15,26 @@ const getAllGuides = async (req, res, next) => {
   });
 };
 
+// Get a guide's public info & services by username
+const getGuideInfoByUsername = async (req, res, next) => {
+  const { username } = req.params;
+
+  const guide = await guideService.getGuideByUsername(username);
+
+  if (!guide) {
+    return next(new ApiError(httpStatus.notFound, "Guide not found"));
+  }
+
+  const services = await guideService.getGuideServices(guide._id);
+
+  res.status(httpStatus.ok).json({
+    success: true,
+    guide,
+    services,
+  });
+};
+// Export the controller functions
 module.exports = {
-  getAllGuides
+  getAllGuides,
+    getGuideInfoByUsername,
 };
