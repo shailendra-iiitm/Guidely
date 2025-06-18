e7a47a52cde27d60c0872e3cccd7fb103fb5cd83
SHAILENDRA SHUKLA <SHAILENDRAIIITS@GMAIL.COM>
2025-06-18 11:20:45 +0530

    AVAILABILITY Module Completed

---
+++ backend/validations/availability.validation.js
@@ -0,0 +1,27 @@
+// backend/validations/availability.validation.js
+
+const Joi = require("joi");
+
+const timeSlotSchema = Joi.object({
+  startTime: Joi.string().required(),
+  endTime: Joi.string().required(),
+});
+
+const weeklyAvailabilitySchema = Joi.object({
+  monday: Joi.array().items(timeSlotSchema).optional(),
+  tuesday: Joi.array().items(timeSlotSchema).optional(),
+  wednesday: Joi.array().items(timeSlotSchema).optional(),
+  thursday: Joi.array().items(timeSlotSchema).optional(),
+  friday: Joi.array().items(timeSlotSchema).optional(),
+  saturday: Joi.array().items(timeSlotSchema).optional(),
+  sunday: Joi.array().items(timeSlotSchema).optional(),
+});
+
+const createAvailabilityValidation = Joi.object({
+  weeklyAvailability: weeklyAvailabilitySchema.required(),
+  unavailableDates: Joi.array().items(Joi.date()).optional(),
+});
+
+module.exports = {
+  createAvailabilityValidation,
+};
