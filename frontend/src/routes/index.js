24741b6d3fc6eaf7d8629d2f5bd152688fdb98bb
SHAILENDRA SHUKLA <SHAILENDRAIIITS@GMAIL.COM>
2025-06-23 15:53:15 +0530

    Added Additional routes for dashboard

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -6,7 +6,13 @@ import AllGuides from "../page/allGuides";
 import GuideDetails from "../page/guideDetails";
 import Booking from "../page/booking";
 import SuccessPage from "../page/successPage";
-
+import BookingPage from "../page/bookingPage";
+import Dashboard from "../page/dashboard";
+import Services from "../page/dashboard/services";
+import Profile from "../page/dashboard/profile";
+import Schedule from "../page/dashboard/schedule";
+import Payment from "../page/dashboard/payment";
+import Bookings from "../page/dashboard/bookings";
 
 const routes = [
   {
@@ -54,6 +60,36 @@ const routes = [
     path: "*",
     element: <PageNotFound />,
     isProtected: false,
+  },
+   {
+    path: "/dashboard",
+    element: <Dashboard />,
+    isProtected: true,
+  },
+  {
+    path: "/dashboard/services",
+    element: <Services />,
+    isProtected: true,
+  },
+  {
+    path: "/dashboard/profile",
+    element: <Profile />,
+    isProtected: true,
+  },
+  {
+    path: "/dashboard/schedule",
+    element: <Schedule />,
+    isProtected: true,
+  },
+  {
+    path: "/dashboard/payment",
+    element: <Payment />,
+    isProtected: true,
+  },
+  {
+    path: "/dashboard/bookings",
+    element: <Bookings />,
+    isProtected: true,
   },
 ];
 
